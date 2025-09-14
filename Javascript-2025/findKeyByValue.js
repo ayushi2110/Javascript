1e575fe04e4043a284cfdb7867c591ec29478263
ayushi2110 <ayushikhurana2110@gmail.com>
2025-09-14 12:57:23 +0530

    feat: find key by value and return array

--- Javascript-2025/findKeyByValue.js
+++ Javascript-2025/findKeyByValue.js
@@ -7,6 +7,10 @@ let objectValues = {
   f: "banana",
 };
 
+// This code finds all keys in an object that have a specific value.
+// The findKeyByValue function returns an array of keys where the value matches.
+// Example: For value "apple", it returns ['a', 'e']
+
 function findKeyByValue(obj, value) {
   return Object.keys(obj).filter((key) => obj[key] === value);
 }
