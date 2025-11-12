2adbc0c1e71c69f7935652e6a551b60c05e81a4a
ayushi2110 <ayushikhurana2110@gmail.com>
2025-11-12 18:59:23 +0530

    feat: check palindrome

---
+++ Javascript-2025/plainDropalindrome.js
@@ -0,0 +1,27 @@
+function checkPalindrome(str) {
+  if (str.length <= 1) return str;
+  let longest = "";
+  function expand(left, right) {
+    while (left >= 0 && right < str.length && str[left] === str[right]) {
+      left--;
+      right++;
+    }
+    return str.slice(left + 1, right);
+  }
+  for (let i = 0; i < str.length; i++) {
+    let odd = expand(i, i); // start from (0,0), (1,1), (2,2)...
+    let even = expand(i, i + 1); // start from (0,1), (1,2), (2,3)...
+    let current = odd.length > even.length ? odd : even;
+    if (current.length > longest.length) {
+      longest = current;
+    }
+  }
+  return longest;
+}
+
+console.log(checkPalindrome("edfabbaij"));
+
+// b = b
+// b = a
+// a = a
+// a = b
